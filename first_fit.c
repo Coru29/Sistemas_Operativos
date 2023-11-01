fde5a79f8a70abea4518a13c834e726efbee7a89
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-11-01 14:09:21 -0300

    Simplificando uso de funcion decimal_a_binario

--- first_fit.c
+++ first_fit.c
@@ -1,25 +1,26 @@
 #include "my_malloc_manager.h"
 
-void decimal_a_binario(unsigned char *bitmap, int *binario, int index){
-    for (int i = 7; i >= 0; i--) {
-        binario[index + i] = *bitmap & 1;
-        *bitmap >>= 1;
+void lista_decimal_a_binario(unsigned char *bitmap, int *lista_en_binario, size_t bitmap_size){
+
+    for (int i = 0; i < bitmap_size; i++) {
+        for (int j = 7; j >= 0; j--) {
+            lista_en_binario[(i * 8) + j] = *bitmap & 1;
+            *bitmap >>= 1;
+        }
     }
+
 }
 
 // Esta función busca un hueco de tamaño units_needed en el bitmap.
 int first_fit(unsigned char *bitmap, size_t bitmap_size, size_t units_needed) {
     
-    int enbinario[bitmap_size * 8];
+    int bitmap_en_binario[bitmap_size * 8];
 
 
-    for (int i = 0; i < bitmap_size; i++) {
-        decimal_a_binario(bitmap, enbinario, i * 8);
+    lista_decimal_a_binario(bitmap, bitmap_en_binario, bitmap_size);
+
+    for (size_t i = 0; i < bitmap_size * 8; i++){
+        printf("%i",bitmap_en_binario[i]);
     }
-
-    for (size_t i = 0; i < bitmap_size * 8; i++)
-    {
-        printf("%i",enbinario[i]);
-    }
-    
+    printf("\n");
 }
