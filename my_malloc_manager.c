4a855412be40a67d76ce2309a4f22ae2f241e18b
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-11-01 16:11:58 -0300

    Agregando un par de comentarios

--- my_malloc_manager.c
+++ my_malloc_manager.c
@@ -14,20 +14,24 @@ void print_bitmap(unsigned char *bitmap, size_t bitmap_size) {
 }
 
 int main() {
-    // 1. Definir el bitmap.
+    // Definiendo el bitmap.
+    // Este array va a tener elementos que pueden ir de 0 a 255,
+    // esto es genial para representar el bitmap, ya que la representacion 
+    // binaria de cada elemento tiene 8 bits.
     unsigned char bitmap [BITMAP_SIZE];
 
-    // 2. Inicializar el bitmap con ceros.
+    // Inicializando el bitmap con ceros.
     for (size_t i = 0; i < BITMAP_SIZE; i++) {
-        //bitmap[i] = {0, 0, 0, 0, 0, 0, 0};
         bitmap[i] = 0;
     }
-    bitmap[0] = 11;
-    // 3. Imprimir el bitmap.
-    //printf("Bitmap inicial: ");
-    //print_bitmap(bitmap, BITMAP_SIZE);
+    bitmap[0] = 111;
+    // Imprimiendo el bitmap.
+    printf("Bitmap inicial: ");
+
+    print_bitmap(bitmap, BITMAP_SIZE);
 
     first_fit(bitmap, BITMAP_SIZE, 10);
+
     // 4. Hacer varias invocaciones de first_fit y mostrar el resultado.
     int indices[] = {5, 10, 7, 20, 50, 2};  // Ejemplo de tamaños solicitados
     for (int i = 0; i < sizeof(indices) / sizeof(indices[0]); i++) {
