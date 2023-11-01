c3b362bef62e201d0ff4ed9f6b0b7ea158c6b2bf
lucaspintos909 <lucaspintos909@gmail.com>
2023-10-31 23:55:14 -0300

    Funcion para pasar bitmap de decimal a binario

--- first_fit.c
+++ first_fit.c
@@ -1,13 +1,26 @@
 #include "my_malloc_manager.h"
 
+void decimal_a_binario(unsigned char bitmap, int *binario, int index){
+    
+    for (int i = 7; i >= 0; i--) {
+        binario[index + i] = bitmap & 1;
+        bitmap >>= 1;
+    }
+}
 
 // Esta función busca un hueco de tamaño units_needed en el bitmap.
 int first_fit(unsigned char *bitmap, size_t bitmap_size, size_t units_needed) {
-    char cosas [BITMAP_SIZE * 8];
     
-   for (size_t i = 0; i < bitmap_size; i++) {
-        for (int j = 0; j < 8; j++) {
-            cosas[j + i * 8] = (bitmap[i] >> j) & 1;
-        }
+    int enbinario[bitmap_size * 8];
+
+
+    for (int i = 0; i < bitmap_size; i++) {
+        decimal_a_binario(*bitmap, enbinario, i * 8);
     }
+
+    for (size_t i = 0; i < bitmap_size * 8; i++)
+    {
+        printf("%i",enbinario[i]);
+    }
+    
 }
