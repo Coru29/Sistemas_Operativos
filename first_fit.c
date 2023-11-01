e8fe689838b67cfdb74cdba914123c054cf6c755
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-11-01 13:44:32 -0300

    Arreglando funcion decimal_a_binario

--- first_fit.c
+++ first_fit.c
@@ -1,10 +1,9 @@
 #include "my_malloc_manager.h"
 
-void decimal_a_binario(unsigned char bitmap, int *binario, int index){
-    
+void decimal_a_binario(unsigned char *bitmap, int *binario, int index){
     for (int i = 7; i >= 0; i--) {
-        binario[index + i] = bitmap & 1;
-        bitmap >>= 1;
+        binario[index + i] = *bitmap & 1;
+        *bitmap >>= 1;
     }
 }
 
@@ -15,7 +14,7 @@ int first_fit(unsigned char *bitmap, size_t bitmap_size, size_t units_needed) {
 
 
     for (int i = 0; i < bitmap_size; i++) {
-        decimal_a_binario(*bitmap, enbinario, i * 8);
+        decimal_a_binario(bitmap, enbinario, i * 8);
     }
 
     for (size_t i = 0; i < bitmap_size * 8; i++)
