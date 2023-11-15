8cca9f7561152c32d04f1c3fc83528af9705e309
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-11-15 19:00:23 -0300

    Declarando MemoryChunkHeader y Allocation Header

--- src/my_malloc_manager.h
+++ src/my_malloc_manager.h
@@ -8,8 +8,12 @@
 #include <stdlib.h> // for EXIT_FAILURE
 #include <string.h> // for string manipulation functions
 #include <stdint.h> // for uint16_t
+#include <sys/mman.h> // for uint16_t
 
-#define BITMAP_SIZE 16 // in bytes
+
+#define BITMAP_SIZE (uint16_t)16 // in bytes
+#define UNIT_SIZE (uint16_t)16   // minimum unit to assign, in bytes
+#define UNITS_PER_CHUNK (uint16_t)(BITMAP_SIZE * 8)
 
 #endif // MY_ALLOC_MANAGER_H
 
@@ -22,3 +26,23 @@ int clear_bits(Bitmap bitmap, uint16_t start_byte_index, uint16_t start_bit_inde
 // Utils
 void lista_decimal_a_binario(unsigned char *bitmap, char *lista_en_binario, size_t bitmap_size);
 void lista_binario_a_decimal(unsigned char *bitmap, char *lista_en_binario, size_t bitmap_size);
+
+// Chunk
+typedef struct MemoryChunkHeader // All chunks have this header
+{
+    uint16_t id;                    // id of the chunk, useful for debugging
+    uint16_t is_large_allocation;   // Flag to indicate if this is for a single large allocation
+    uint16_t chunk_total_units;     // Size of the memory block in units
+    uint16_t chunk_available_units; // How many units are available
+    Bitmap bitmap;                  // Pointer to the bitmap for this chunk, NULL if is_large_allocation
+    uint16_t bitmap_size;           // Size of bitmap in bytes, 0 if is_large_allocation
+    struct MemoryChunkHeader *next; // Pointer to the next MemoryChunkHeader, NULL if last one
+} MemoryChunkHeader;
+
+
+// Allocation Header
+typedef struct AllocationHeader // All malloc calls have a this header located before the returned address
+{
+    uint16_t nunits;    // number of units
+    uint16_t bit_index; // offset from the MemoryChunkHeader struct
+} AllocationHeader;
